3aab955720e2765a275222fe053374635ca274c6
v-like-engine <cypepv@yandex.ru>
2020-01-10 01:13:49 +0500

    Initial commit

---
+++ py_ui.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'UI.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QtCore.QSize(800, 600))
+        MainWindow.setMaximumSize(QtCore.QSize(1920, 1080))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 520, 781, 71))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        font.setItalic(True)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Yellow Circles"))
+        self.pushButton.setText(_translate("MainWindow", "Add Circle"))
---
+++ yellc.py
@@ -0,0 +1,40 @@
+import random
+import sys
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from py_ui import Ui_MainWindow
+
+
+class MainW(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.circles = []
+        self.draw = False
+        self.pushButton.clicked.connect(self.random_circle)
+
+    def random_circle(self):
+        self.draw = True
+        self.circles.append([(random.randrange(0, 800), random.randrange(0, 500)), random.randrange(10, 300)])
+        self.update()
+
+    def paintEvent(self, event):
+        if self.draw:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor(255, 255, 0))
+            for el in self.circles:
+                self.draw_circle(qp, el)
+            qp.end()
+
+    def draw_circle(self, qp, params):
+        qp.drawEllipse(*params[0], params[1], params[1])
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_w = MainW()
+    main_w.show()
+    sys.exit(app.exec())
